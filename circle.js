a01e2a5a8b514fcb62d113afdc2953c6aa2ee035
Williams, Cameron <cameron.williams@hawaiianair.com>
2021-02-11 16:58:34 -0700

    Setup UI for basic functions of the game

---
+++ circle.js
@@ -0,0 +1,33 @@
+class circle{
+    radius;
+    x;
+    y;
+
+    constructor(radius,x,y){
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    contains(x,y){
+        return Math.sqrt(Math.pow((x - this.x), 2) + Math.pow((y - this.y), 2)) < this.radius;
+    }
+
+    draw(context, player){
+        console.log("X: " + this.x);
+        console.log("y: " + this.y);
+        context.beginPath();
+        context.arc(this.x,this.y,this.radius, 0, 2* Math.PI);
+        context.stroke();
+        if(player == gameBoard.player.PLAYER_ONE){
+            context.fillStyle = 'orange';
+            context.fill();
+        }else if(player == gameBoard.player.PLAYER_TWO){
+            context.fillStyle = "blue";
+            context.fill();
+        }else{
+            context.stroke();
+        }
+    }
+}
