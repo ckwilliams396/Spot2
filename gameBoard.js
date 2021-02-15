91e15d7a2c0e6a425f73ee0e58603745e58a29f1
Sean Tunell <stunell@catalyte.io>
2021-02-15 15:16:47 -0700

    one line initialization

--- gameBoard.js
+++ gameBoard.js
@@ -9,45 +9,41 @@ class gameBoard{
     playerTurn = gameBoard.player.PLAYER_ONE;
 
     constructor(){
-        this.board = new Array(this.boardSize).fill(new Array(this.boardSize).fill(new Token(gameBoard.player.EMPTY)))
-        this.board[0][0].setPlayer(player.PLAYER_ONE)
-        this.board[this.boardSize-1][this.boardSize-1].setPlayer(player.PLAYER_ONE)
-        this.board[0][this.boardSize-1].setPlayer(player.PLAYER_TWO)
-        this.board[this.boardSize-1][0].setPlayer(player.PLAYER_TWO)
+        this.board = new Array(this.boardSize).fill().map(b => {return new Array(this.boardSize).fill().map(a => {return new Token(gameBoard.player.EMPTY)})});
+        this.board[0][0].setPlayer(gameBoard.player.PLAYER_ONE)
+        this.board[this.boardSize-1][this.boardSize-1].setPlayer(gameBoard.player.PLAYER_ONE)
+        this.board[0][this.boardSize-1].setPlayer(gameBoard.player.PLAYER_TWO)
+        this.board[this.boardSize-1][0].setPlayer(gameBoard.player.PLAYER_TWO)
     }
 
     hasWinner(){
-        let emptyCount = 0;
+        // no empty spots available
         for(let i=0; i<this.boardSize; i++){
             for (let j=0; i<this.boardSize; j++){
                 if(board[i][j].player == this.player.EMPTY) return false;
             }
         }
         return true;
+        // Only one player has tokens on the board
     }
 
     isValidMove(fromX, fromY, toX, toY){
         let deltaX = Math.abs(toX - fromX);
         let deltaY = Math.abs(toY - fromY);
-        return deltaX + deltaY <= 3 && (deltaX <= 2 && deltaY <= 2);
+        let isSkipMove = deltaX == 2 || deltaY == 2;
+        let isValidMove = deltaX + deltaY <= 4 && (deltaX <= 2 && deltaY <= 2) && this.board[toX][toY].getPlayer() == gameBoard.player.EMPTY;
+        return {isSkipMove, isValidMove};
     }
-
-    isSkipMove(fromX, fromY, toX, toY){
-        let deltaX = Math.abs(toX - fromX);
-        let deltaY = Math.abs(toY - fromY);
-        return deltaX == 2 || deltaY == 2;
-    }
-
 
     moveToken(fromX, fromY, toX, toY){
+        // currentPlayer should be known!
         let currentPlayer = this.board[fromX][fromY].getPlayer();
-        if(this.isValidMove(fromX, fromY, toX, toY)){
+        let {isValidMove, isSkipMove} = this.isValidMove(fromX, fromY, toX, toY);
+        if(isValidMove){
             this.board[toX][toY].setPlayer(currentPlayer);
-            if(this.isSkipMove(fromX, fromY, toX, toY)){
-                this.board[fromX][fromY].setPlayer(gameBoard.player.EMPTY);
-            }
+            isSkipMove && this.board[fromX][fromY].setPlayer(gameBoard.player.EMPTY);
             this.claim(toX,toY, currentPlayer);
-        }else{
+        } else {
             console.log("invalid move");
         }
     }
